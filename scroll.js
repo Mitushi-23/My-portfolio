058bedb941a28dc64743a3e82ed35e21dd5f79ba
mitushi <mitushi2agrawal@gmail.com>
2021-11-27 02:07:48 +0530

    first page completed

--- scroll.js
+++ scroll.js
@@ -1,54 +1,88 @@
 // Detect request animation frame
-var scroll = window.requestAnimationFrame ||
-             // IE Fallback
-             function(callback){ window.setTimeout(callback, 1000/60)};
-var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+var scroll =
+  window.requestAnimationFrame ||
+  // IE Fallback
+  function (callback) {
+    window.setTimeout(callback, 1000 / 60)
+  }
+var elementsToShow = document.querySelectorAll('.show-on-scroll')
 
 function loop() {
+  Array.prototype.forEach.call(elementsToShow, function (element) {
+    if (isElementInViewport(element)) {
+      element.classList.add('is-visible')
+    } else {
+      element.classList.remove('is-visible')
+    }
+  })
 
-    Array.prototype.forEach.call(elementsToShow, function(element){
-      if (isElementInViewport(element)) {
-        element.classList.add('is-visible');
-      } else {
-        element.classList.remove('is-visible');
-      }
-    });
-
-    scroll(loop);
+  scroll(loop)
 }
 
 // Call the loop for the first time
-loop();
+loop()
 
 // Helper function from: http://stackoverflow.com/a/7557433/274826
 function isElementInViewport(el) {
   // special bonus for those using jQuery
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-    el = el[0];
+  if (typeof jQuery === 'function' && el instanceof jQuery) {
+    el = el[0]
   }
-  var rect = el.getBoundingClientRect();
+  var rect = el.getBoundingClientRect()
   return (
-    (rect.top <= 0
-      && rect.bottom >= 0)
-    ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-    ||
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <=
+        (window.innerHeight || document.documentElement.clientHeight)) ||
     (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-  );
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight))
+  )
 }
-
-// function move() {
-//   var elem = document.getElementById("progress-bar");
-//   var width = 1;
-//   var id = setInterval(frame, 10);
-//   function frame() {
-//     if (width >= 100) {
-//       clearInterval(id);
-//     } else {
-//       width++;
-//       elem.style.width = width + '%';
-//     }
-//   }
-// }
+function progress() {
+  $('.p1').animate(
+    {
+      width: '25%',
+    },
+    1000,
+  )
+  $('.p2').animate(
+    {
+      width: '50%',
+    },
+    1000,
+  )
+  $('.p3').animate(
+    {
+      width: '75%',
+    },
+    1000,
+  )
+  $('.p4').animate(
+    {
+      width: '93%',
+    },
+    1000,
+  )
+  $('.p5').animate(
+    {
+      width: '85%',
+    },
+    1000,
+  )
+  $('.p6').animate(
+    {
+      width: '90%',
+    },
+    1000,
+  )
+}
+window.load()
+{
+  progress()
+}
+function load() 
+{
+  progress()
+}
